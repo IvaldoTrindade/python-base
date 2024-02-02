dcbb478f03c294a0dde219856d8102fb358e69de
Ivaldo <ivaldotrindade1245@gmail.com>
2024-02-02 10:04:42 -0300

    Inserindo comentários dentro do documento

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
+#!/usr/bin/env python3
+
+#Existe o SheBang, que é um comentário que serve para ser interpretado em alguns OS, e deve ser inserido sempre na primeira linha. 
+
+#Eu estou inserindo um comentário nesta linha. Para fazer isso, eu simplemtente devo inserir o "#" 
 print("Ivaldo".upper())
